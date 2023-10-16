b4251861ddf7ce95f5ef756f5a18772747fc4bd4
romangojshik <romangojshik2@gmail.com>
2023-10-16 17:00:49 +0300

    init project

---
+++ To-Do List App/To-Do List App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-Do List App
+//
+//  Created by Roman on 10/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
